9dfdf11c16f65c6a4ca0507111d08d2965933b29
Yuki-w6 <m1610600@gmail.com>
2024-09-12 14:26:56 +0000

    layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
+import './ui/global.css';
+import { Inconsolata } from 'next/font/google';
+
+// Googleフォントを有効化
+const fnt = Inconsolata({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'reduce image size',
+  description: '画像サイズを下げるためのアプリ',
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,8 +18,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
-      <div>test</div>
+      <body className={fnt.className}>
+        <h1 className="text-4xl text-indigo-800 font-bold my-2">
+          画像サイズを下げる
+        </h1>
+        <ul className="flex bg-blue-600 mb-4 pl-2">
+          <li className="block px-4 py-2 my-1 hover:bg-gray-100 rounded">
+            <Link className="no-underline text-blue-300" href="/">
+              Home
+            </Link>
+          </li>
+        </ul>
+        <div className="ml-2">{children}</div>
+      </body>
     </html>
   );
 }
